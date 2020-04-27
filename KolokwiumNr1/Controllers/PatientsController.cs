884961f32c6372f01b340ae3ab59ba4cc3bad0b8
s18371 <s18371@pjwstk.edu.pl>
2020-04-27 17:16:16 +0200

    Kolos nr 1

---
+++ KolokwiumNr1/Controllers/PatientsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using KolokwiumNr1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KolokwiumNr1.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class PatientsController : ControllerBase
+    {
+        private const string ConnString = "Data Source=db-mssql;Initial Catalog=s18371;Integrated Security=True";
+        public IConfiguration Configuration { get; set; }
+        [Route("medicaments")]
+        // GET: api/<controller>
+        [HttpGet("{id}")]
+        public IActionResult GetMedicament(string id)
+        {
+            var listaP = new List<Persctriprion>();
+
+            using (SqlConnection con = new SqlConnection(ConnString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                con.Open();
+                com.CommandText = "select idmedicament from medicament where idmedicament = @id";
+                com.Parameters.AddWithValue("id", id);
+                var dr = com.ExecuteReader();
+                int jest = 0;
+                if (dr.Read())
+                {
+                    jest = (int)dr["IdMedicament"];
+                }
+                if (jest == 0)
+                {
+                    return BadRequest("Nie ma takiego leku");
+                }
+                dr.Close();
+                com.CommandText = "select Prescription.IdPrescription, Prescription.Date, Prescription.DueDate, Prescription.IdPatient, Prescription.IdDoctor, Prescription_Medicament.Dose, Prescription_Medicament.Details from Prescription join Prescription_Medicament on Prescription.IdPrescription = Prescription_Medicament.IdPrescription join Medicament on Prescription_Medicament.IdMedicament = Medicament.IdMedicament where Medicament.IdMedicament = @id order by date desc";
+                com.Parameters.AddWithValue("id", id);
+                dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    var listaLekow = new Persctriprion();
+                    listaLekow.id = (int)dr["IdPrescription"];
+                    listaLekow.date = dr["Date"].ToString();
+                    listaLekow.dueDtate = dr["DueDate"].ToString();
+                    listaLekow.idPat = (int)dr["idPatient"];
+                    listaLekow.idDoc = (int)dr["IdDoctor"];
+                    listaLekow.dose = (int)dr["Dose"];
+                    listaLekow.details = dr["Details"].ToString();
+                    listaP.Add(listaLekow);
+                }
+                dr.Close();
+
+            }
+            return Ok(listaP);
+        }
+
+      
+        [Route("patients")]
+        [HttpDelete("{id}")]
+        public IActionResult DeletePatient(string id)
+        {
+            using (SqlConnection con = new SqlConnection(ConnString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                con.Open();
+                com.Connection = con;
+                SqlTransaction trans = con.BeginTransaction();
+                com.Transaction = trans;
+           
+                com.CommandText = "select * from patient where idPatient = @id";
+                com.Parameters.AddWithValue("id", id);
+                var dr = com.ExecuteReader();
+                int jest = 0;
+                if (dr.Read())
+                {
+                    jest = (int)dr["idPatient"];
+                }
+                dr.Close();
+                if (jest == 0)
+                {
+                    return BadRequest("nie ma takiego pacjenta");
+                }
+                else
+                {
+                    com.CommandText = "select * from Prescription where idPatient = @id";
+                    com.Parameters.AddWithValue("id", id);
+                    dr = com.ExecuteReader();
+                    jest = 0;
+                    if (dr.Read())
+                    {
+                        jest = (int)dr["IdPrescription"];
+                    }
+                }
+                if (jest == 0)
+                {
+                    try
+                    {
+                        com.CommandText = "delete from patient where idpatient=@id";
+                        com.Parameters.AddWithValue("id", id);
+                        com.ExecuteNonQuery();
+                        trans.Commit();
+                    }catch(Exception e)
+                    {
+                        trans.Rollback();
+                        return BadRequest("Wystapil blad");
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        com.CommandText = "delete from Prescription where idPatient=@id";
+                        com.Parameters.AddWithValue("id", id);
+                        com.ExecuteNonQuery();
+                        com.CommandText = "delete forom Patient where idPatient=@id";
+                        com.Parameters.AddWithValue("id", id);
+                        com.ExecuteNonQuery();
+                        trans.Commit();
+                    }catch(Exception e)
+                    {
+                        trans.Rollback();
+                        return BadRequest("Wystapil blad");
+                    }
+                }
+
+                return Ok("Usunięto pacjenta i jego recepty");
+            }
+        }
+    }
+}
---
+++ KolokwiumNr1/Models/Persctriprion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolokwiumNr1.Models
+{
+    public class Persctriprion
+    {
+        public int id { get; set; }
+        public string date { get; set; }
+        public string dueDtate { get; set; }
+        public int idPat { get; set; }
+        public int idDoc { get; set; }
+        public int dose { get; set; }
+        public string details { get; set; }
+    }
+}
