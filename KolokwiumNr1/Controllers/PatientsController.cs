02d4f9c3f3bca3e19954ae65cb6b97ef4c888f30
s18371 <s18371@pjwstk.edu.pl>
2020-04-27 17:33:51 +0200

    Prosty błąd....

--- KolokwiumNr1/Controllers/PatientsController.cs
+++ KolokwiumNr1/Controllers/PatientsController.cs
@@ -12,27 +12,27 @@ using Microsoft.Extensions.Configuration;
 namespace KolokwiumNr1.Controllers
 {
     [ApiController]
-    [Route("api")]
+    [Route("api/medicaments")]
     public class PatientsController : ControllerBase
     {
         private const string ConnString = "Data Source=db-mssql;Initial Catalog=s18371;Integrated Security=True";
         public IConfiguration Configuration { get; set; }
-        [Route("medicaments")]
-        // GET: api/<controller>
+        //[Route("medicaments")]
         [HttpGet("{id}")]
         public IActionResult GetMedicament(string id)
         {
             var listaP = new List<Persctriprion>();
-
             using (SqlConnection con = new SqlConnection(ConnString))
             using (SqlCommand com = new SqlCommand())
             {
+            
                 com.Connection = con;
                 con.Open();
                 com.CommandText = "select idmedicament from medicament where idmedicament = @id";
                 com.Parameters.AddWithValue("id", id);
                 var dr = com.ExecuteReader();
                 int jest = 0;
+                
                 if (dr.Read())
                 {
                     jest = (int)dr["IdMedicament"];
@@ -42,8 +42,8 @@ namespace KolokwiumNr1.Controllers
                     return BadRequest("Nie ma takiego leku");
                 }
                 dr.Close();
-                com.CommandText = "select Prescription.IdPrescription, Prescription.Date, Prescription.DueDate, Prescription.IdPatient, Prescription.IdDoctor, Prescription_Medicament.Dose, Prescription_Medicament.Details from Prescription join Prescription_Medicament on Prescription.IdPrescription = Prescription_Medicament.IdPrescription join Medicament on Prescription_Medicament.IdMedicament = Medicament.IdMedicament where Medicament.IdMedicament = @id order by date desc";
-                com.Parameters.AddWithValue("id", id);
+                com.CommandText = "select Prescription.IdPrescription, Prescription.Date, Prescription.DueDate, Prescription.IdPatient, Prescription.IdDoctor, Prescription_Medicament.Dose, Prescription_Medicament.Details from Prescription join Prescription_Medicament on Prescription.IdPrescription = Prescription_Medicament.IdPrescription join Medicament on Prescription_Medicament.IdMedicament = Medicament.IdMedicament where Medicament.IdMedicament = @id2 order by date desc";
+                com.Parameters.AddWithValue("id2", id);
                 dr = com.ExecuteReader();
                 while (dr.Read())
                 {
@@ -104,8 +104,8 @@ namespace KolokwiumNr1.Controllers
                 {
                     try
                     {
-                        com.CommandText = "delete from patient where idpatient=@id";
-                        com.Parameters.AddWithValue("id", id);
+                        com.CommandText = "delete from patient where idpatient=@id2";
+                        com.Parameters.AddWithValue("id2", id);
                         com.ExecuteNonQuery();
                         trans.Commit();
                     }catch(Exception e)
@@ -118,11 +118,11 @@ namespace KolokwiumNr1.Controllers
                 {
                     try
                     {
-                        com.CommandText = "delete from Prescription where idPatient=@id";
-                        com.Parameters.AddWithValue("id", id);
+                        com.CommandText = "delete from Prescription where idPatient=@id3";
+                        com.Parameters.AddWithValue("id3", id);
                         com.ExecuteNonQuery();
-                        com.CommandText = "delete forom Patient where idPatient=@id";
-                        com.Parameters.AddWithValue("id", id);
+                        com.CommandText = "delete forom Patient where idPatient=@id4";
+                        com.Parameters.AddWithValue("id4", id);
                         com.ExecuteNonQuery();
                         trans.Commit();
                     }catch(Exception e)
